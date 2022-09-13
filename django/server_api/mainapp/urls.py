73151f6d95a4ca7a468cbbf6ff1c7d832d5ededb
Sanetro <sanetro26@gmail.com>
2022-09-13 21:45:04 +0200

    REST API done

--- django/server_api/mainapp/urls.py
+++ django/server_api/mainapp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('artifacts/', views.showArtifacts),
+    path('bossMaterial/', views.showBossMaterial),
+    path('characterAscension/', views.showCharacterAscension),
+    path('characters/', views.showCharacters),
+    path('commonAscension/', views.showCommonAscension),
+    path('materialsTalentBook/', views.showMaterialsTalentBook),
+    path('weapons/', views.showWeapons),
 ]
--- django/server_api/mainapp/views.py
+++ django/server_api/mainapp/views.py
@@ -1,6 +1,67 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+import requests as re
 # Create your views here.
 
-def say_hello(request):
-    return HttpResponse('Hello')
+def showArtifacts(request):
+    artifacts = re.get("https://api.genshin.dev/artifacts")
+    artifactsResult = artifacts.json()
+    res = {
+        'success': True,
+        'artifacts': artifactsResult
+    }
+    return JsonResponse(res)
+
+def showWeapons(request):
+    weapons = re.get("https://api.genshin.dev/weapons")
+    weaponsResult = weapons.json()
+    res = {
+        'success': True,
+        'artifacts': weaponsResult
+    }
+    return JsonResponse(res)
+
+def showCharacters(request):
+    characters = re.get("https://api.genshin.dev/characters")
+    charactersResult = characters.json()
+    res = {
+        'success': True,
+        'artifacts': charactersResult
+    }
+    return JsonResponse(res)
+
+def showMaterialsTalentBook(request):
+    materialsTalentBook = re.get("https://api.genshin.dev/materials/talent-book")
+    materialsTalentBookResult = materialsTalentBook.json()
+    res = {
+        'success': True,
+        'artifacts': materialsTalentBookResult
+    }
+    return JsonResponse(res)
+
+def showBossMaterial(request):
+    bossMaterial = re.get("https://api.genshin.dev/materials/boss-material")
+    bossMaterialResult = bossMaterial.json()
+    res = {
+        'success': True,
+        'artifacts': bossMaterialResult
+    }
+    return JsonResponse(res)
+
+def showCharacterAscension(request):
+    characterAscension = re.get("https://api.genshin.dev/materials/character-ascension")
+    characterAscensionResult = characterAscension.json()
+    res = {
+        'success': True,
+        'artifacts': characterAscensionResult
+    }
+    return JsonResponse(res)
+
+def showCommonAscension(request):
+    commonAscension = re.get("https://api.genshin.dev/materials/common-ascension")
+    commonAscensionResult = commonAscension.json()
+    res = {
+        'success': True,
+        'artifacts': commonAscensionResult
+    }
+    return JsonResponse(res)
